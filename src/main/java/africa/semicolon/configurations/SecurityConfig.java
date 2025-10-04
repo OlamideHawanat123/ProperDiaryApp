21b631e340dca699e2b0beeb79e46bd5758796bd
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-10-04 22:52:47 +0100

    started spring security

---
+++ src/main/java/africa/semicolon/configurations/SecurityConfig.java
@@ -0,0 +1,16 @@
+package africa.semicolon.configurations;
+
+import org.springframework.context.annotation.Bean;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+    SecurityFilterCh
+}
--- src/main/java/africa/semicolon/controller/UserController.java
+++ src/main/java/africa/semicolon/controller/UserController.java
@@ -1,4 +1,30 @@
 package africa.semicolon.controller;
 
+import africa.semicolon.DTOs.requests.RegisterUserRequest;
+import africa.semicolon.DTOs.responses.RegisterUserResponse;
+import africa.semicolon.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user/")
 public class UserController {
+    @Autowired
+    private UserServices userService;
+
+    @PostMapping("register")
+    public ResponseEntity<?> registerUser(RegisterUserRequest registerUserRequest){
+        try{
+            RegisterUserResponse response = userService.registerUser(registerUserRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }catch(EmptyResultDataAccessException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+    }
 }
